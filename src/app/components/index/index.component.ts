0d9b96135669b6ea7b3c116e9bc85b32fc5d0eaf
maha-app1 <maha.suguru@gmail.com>
2018-05-26 11:03:37 +0530

    first commit

---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,32 @@
+import {CoinService} from "./../../coin.service";
+import {Component, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  coins: any;
+
+  constructor(private http: HttpClient, private service: CoinService) {
+  }
+
+  ngOnInit() {
+    this.getCoins();
+  }
+
+  getCoins() {
+    this.service.getCoins().subscribe(res => {
+      this.coins = res;
+    });
+  }
+
+  deleteCoin(id) {
+    this.service.deleteCoin(id).subscribe(res => {
+      console.log('Deleted');
+    });
+  }
+}
